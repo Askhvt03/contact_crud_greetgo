901c5ccc639c8c5fb9456fb8427da87adb052a1c
Askhvt03 <kaimaskhat@gmail.com>
2024-01-15 22:08:23 +0600

    Create contact CRUD project with postgreSql and mongoDB
    Signed-off-by: Raushan <kaimaskhat@gmail.com>

---
+++ src/main/java/com/example/contactgreetgo/ContactGreetGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.contactgreetgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactGreetGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactGreetGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/contactgreetgo/ContactGreetGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contactgreetgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactGreetGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
